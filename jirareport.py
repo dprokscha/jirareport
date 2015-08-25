d50f4f659369b824609adb7c66d48e8ad28fe833
dprokscha <dp.online@googlemail.com>
2015-08-25 16:26:22 +0200

    added filter by label

--- jirareport.py
+++ jirareport.py
@@ -76,10 +76,38 @@ def burndown(ctx, output=None):
 
     click.secho('OK', fg='green')
 
+    labels = []
+    if click.confirm('Filter issues for labels?'):
+        while True:
+            try:
+                labels.append(click.prompt('Enter label (interrupt to leave)', type=click.STRING))
+            except Exception:
+                break
+
     issues = {}
+    ignored = []
     with click.progressbar(report.all, bar_template='%(label)s [%(bar)s] %(info)s', label='Fetching issues:', show_eta=False) as all:
         for key in all:
-            issues[key] = jira.issue(key, expand='changelog')
+
+            issue = jira.issue(key, expand='changelog')
+
+            if labels and not [label for label in labels if label in issue.fields.labels]:
+                ignored.append(key)
+                continue
+
+            issues[key] = issue
+
+    for key in ignored:
+        if key in report.completed:
+            report.completed.remove(key)
+        if key in report.incompleted:
+            report.incompleted.remove(key)
+        if key in report.added:
+            report.added.remove(key)
+        if key in report.punted:
+            report.punted.remove(key)
+        if key in report.all:
+            report.all.remove(key)
 
     commitment = click.prompt('Enter commitment', type=click.INT)
     burndown = jirareport.report.Burndown(sprint, commitment, report, issues, customfield)
