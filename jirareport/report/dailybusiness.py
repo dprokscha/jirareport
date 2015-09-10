9647874f7127f2f0e5e280b660dd9b0e1e6e3d16
dprokscha <dp.online@googlemail.com>
2015-09-10 16:24:15 +0200

    use sprint instead of single ticket to search for time tracks

--- jirareport/report/dailybusiness.py
+++ jirareport/report/dailybusiness.py
@@ -1,35 +1,32 @@
 import datetime
 import re
 
-def analyse(comments):
+def analyse(sprint, issue):
 
+    start = datetime.datetime.strptime(sprint['startDate'], '%d/%b/%y %I:%M %p')
+    end = datetime.datetime.strptime(sprint['endDate'], '%d/%b/%y %I:%M %p')
     tracks = re.compile('\[([0-9]+)h([0-9]+)m\]')
-    hours = 0
     minutes = 0
 
-    for comment in comments:
+    for comment in issue.fields.comment.comments:
+        created = datetime.datetime.strptime(re.sub(r'\..*$', '', comment.created), '%Y-%m-%dT%H:%M:%S')
+
+        if not (start <= created <= end):
+            continue
+
         for track in tracks.findall(comment.body):
 
             minutes += int(track[0]) * 60
             minutes += int(track[1])
 
+    return minutes
+
+def format(minutes):
+
+    hours = 0
+
     if 0 < minutes:
         hours = int(minutes / 60)
         minutes %= 60
 
-    return {
-        'hours': hours,
-        'minutes': minutes
-    }
-
-def average(length, hours, minutes):
-
-    minutes = int(((hours * 60) + minutes) / length)
-
-    hours = int(minutes / 60)
-    minutes %= 60
-
-    return {
-        'hours': hours,
-        'minutes': minutes
-    }
+    return (hours, minutes)
