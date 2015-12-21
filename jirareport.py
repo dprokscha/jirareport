b168a8a93cf5c47c76e335d2cac954b3101a61b7
dprokscha <dp.online@googlemail.com>
2015-12-21 20:12:33 +0100

    doc

--- jirareport.py
+++ jirareport.py
@@ -7,7 +7,13 @@ import app.jira
 
 
 def load_commands(f):
+    """
+    Loads all modules found in /app/commands as commands for the given
+    click.group() instance. The module name must be equal to the command
+    name. The command callback must be an instance of click.command().
 
+    :param f: Instance of click.group()
+    """
     app_path = os.path.dirname(os.path.realpath(__file__))
     commands_path = os.path.join(app_path, 'app', 'commands')
 
@@ -43,8 +49,16 @@ def load_commands(f):
               prompt=True,
               type=click.STRING)
 @click.pass_context
-def main(ctx, server, username, password, customfield):
+def app(ctx, server, username, password, customfield):
+    """
+    Initializes the CLI interface and the application session.
 
+    :param ctx: Holds the click context object.
+    :param server: Holds the URL to JIRA server.
+    :param username: Holds the JIRA username.
+    :param password: Holds the password.
+    :customfield: Holds JIRA internal field name for issue estimation.
+    """
     jira = app.jira.connect(server, username, password)
     if not jira:
         click.echo('Connection to {0} failed'.format(server), err=True)
@@ -55,4 +69,4 @@ def main(ctx, server, username, password, customfield):
 
 
 if __name__ == '__main__':
-    main()
+    app()
